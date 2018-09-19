375a044280a9b34b5cd8ca681eb699d2fadd03ca
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-09-19 17:53:53 +0200

    Package manager detection

--- install.sh
+++ install.sh
@@ -2,6 +2,27 @@
 
 #Detection of the OS 
 OS_name=`lsb_release -i | awk -F: '{ print $2 }' | awk -F ' ' '{ print $1 }'`
-echo $OS_name
+echo "OS name : $OS_name"
+
+case $OS_name in 
+  
+  "Fedora")
+    package_manager=`which dnf`
+    ;;
+
+  *)
+    package_manager=0
+    ;;
+
+esac
+
+if [ $package_manager != 0 ]
+then 
+  echo "Package Manager : $package_manager"
+else 
+  echo "Sorry ! Your OS isn't compatible yet."
+  echo 1
+fi 
+
 
 exit 0
