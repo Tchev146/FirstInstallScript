fde3bc7f651df122701492d96966eb1d4e4c87b5
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-09-21 08:04:55 +0200

    Adding a list of package given in the script

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 
 #Detection of the OS 
 OS_name=`lsb_release -i | awk -F: '{ print $2 }' | awk -F ' ' '{ print $1 }'`
+echo -e "\n"
 echo "OS name : $OS_name"
 
 case $OS_name in 
   
   "Fedora")
-    package_manager=`which dnf`
+    package_manager="`which dnf` install"
+    ;;
+
+  "Debian" | "Ubuntu" | "Raspbian")
+    package_manager="`which apt-get` install"
     ;;
 
   *)
@@ -16,13 +21,25 @@ case $OS_name in
 
 esac
 
-if [ $package_manager != 0 ]
+if [ "$package_manager" != 0 ]
 then 
-  echo "Package Manager : $package_manager"
+  echo "Package Manager : `echo $package_manager | awk -F" " '{ print $1 }'`"
 else 
   echo "Sorry ! Your OS isn't compatible yet."
-  echo 1
+  exit 1
 fi 
 
+
+echo -e "\n"
+
+package_list="htop powertop tlp git"
+
+for package in $package_list
+do
+  echo "Installing : $package"
+  echo -e "Executing : 'sudo $package_manager $package' \n"
+  #sudo $package_manager $package
+  echo -e "\n"
+done
 
 exit 0
