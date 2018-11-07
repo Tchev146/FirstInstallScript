23d5fac035de6b09f89e9340c0eaefcb5a5d50d6
Kerboit Kevin <kerboit.kevin@gmail.com>
2018-11-07 08:51:38 +0100

    Add some package, add powertop configuration

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # FirstInstallScript
-This script will install some usefull package just after the OS install.
-
-## OS Compatibility
-
-19/09/2018 :
-For now, I design this script for Fedora (dnf). But I will add more later (Debian, etc...) 
-
 
--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#Detection of the OS 
+USER2CREATE="kerboitk"
+
+#Detection of the OS
 OS_name=`lsb_release -i | awk -F: '{ print $2 }' | awk -F ' ' '{ print $1 }'`
 echo -e "\n"
 echo "OS name : $OS_name"
 
-case $OS_name in 
-  
+case $OS_name in
+
   "Fedora")
     package_manager="`which dnf` install"
     ;;
@@ -21,25 +23,72 @@ case $OS_name in
 
 esac
 
-if [ "$package_manager" != 0 ]
-then 
-  echo "Package Manager : `echo $package_manager | awk -F" " '{ print $1 }'`"
-else 
-  echo "Sorry ! Your OS isn't compatible yet."
-  exit 1
-fi 
+CURRENT_USER=`whoami`
 
+if [ "$CURRENT_USER" -ne "root" ]
+then
+  echo "The current user doesn't have permission ! "
+  exit 1
+fi
+
+
+# Creation of the user
+
+User=`grep "$USER2CREATE" /etc/passwd`
+
+if [ $? -eq 0 ]
+then
+  echo "Creating user $USER2CREATE..."
+  useradd $USER2CREATE -m -s /bin/bash
+  passwd $USER2CREATE
+else
+  echo "User $USER2CREATE already created !"
+fi
+
+
+if [ "$package_manager" != 0 ]
+then
+  echo "Package Manager : `echo $package_manager | awk -F" " '{ print $1 }'`"
+else
+  echo "Sorry ! Your OS isn't compatible yet."
+  exit 2
+fi
 
 echo -e "\n"
 
-package_list="htop powertop tlp git"
+package_list="htop powertop tlp git vim curl acpi "
 
 for package in $package_list
 do
   echo "Installing : $package"
-  echo -e "Executing : 'sudo $package_manager $package' \n"
-  #sudo $package_manager $package
+  echo -e "Executing : '$package_manager $package' \n"
+  exec $package_manager $package
   echo -e "\n"
 done
 
+echo "Login as $USER2CREATE"
+su - $USER2CREATE
+
+echo "Installing SpaceVim and his dependancies..."
+curl -sLf https://spacevim.org/install.sh | bash
+
+echo "powertop configuration"
+
+ac_adapter=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
+
+if [ "$ac_adapter" = "off" ]
+then
+  echo -e "This PC has to be on AC, please try-again...\n"
+  exit 3
+fi
+
+sudo powertop --calibrate
+sudo powertop --auto-tune
+
+echo "Generate SSH key"
+pushd ~/.ssh/ > /dev/null
+
+popd > /dev/null
+
 exit 0
+
